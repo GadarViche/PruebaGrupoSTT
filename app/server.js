e275e6996c520bbe06880dec7462e500b0345003
Gabriel <gadarviche@gmail.com>
2022-02-16 22:42:38 -0300

    Actualización

--- app/server.js
+++ app/server.js
@@ -1,53 +1,37 @@
 var express = require('express') //llamamos a Express
 var app = express()
-
+app.use(express.json());
 var port = process.env.PORT || 8080 // establecemos nuestro puerto
+var mysql = require('mysql');
 
-// app.get('/', function (req, res) {
-//   res.json({
-//     mensaje: '¡Hola Mundo!'
-//   })
-// })
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root"
+});
 
-// app.get('/cervezas', function (req, res) {
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
-//   res.json({
-//     mensaje: 'Quiero una cerveza!'
-//   })
 
-// })
+
 
 
 //app.post('/validate-anomaly', function (req, res) {
 app.post('/validate-anomaly', function (req, res) {
 
-  let myJson = JSON.stringify(req.body);
-  console.log("mi variable: " + myJson);
 
-
-  let test01 = req;
-  // let test01 = [
-  //   ["B", "B", "B", "A", "E"],
-  //   ["C", "S", "E", "D", "C"],
-  //   ["A", "C", "B", "F", "E"],
-  //   ["A", "D", "B", "A", "E"]
-  // ];
-
-
-  let test02 = [
-    ["B", "D", "F", "D", "E"],
-    ["A", "B", "C", "D", "B"],
-    ["F", "C", "B", "E", "E"],
-    ["A", "D", "B", "A", "C"]
-  ];
-
-
+  let test01 = req.body.data;
 
   //Mostrar matriz en consola
   for (let x = 0; x < test01.length; x++) {
     console.log(test01[x]);
   }
 
+  let anomalia = false;
+  res.status(403);
   let comentario = "Sin anomalías";
 
 
@@ -56,36 +40,47 @@ app.post('/validate-anomaly', function (req, res) {
     for (let j = 0; j < test01[i].length; j++) {
 
       //Búsqueda de anomalías en Orientación horizontal
-      if (test01[i][j] == test01[i][j + 1] && test01[i][j] == test01[i][j + 2]) {
-        console.log("Largo eje i= " + test01.length + " \nLargo eje j= " + test01[i].length)
+      if (test01[i].length > j + 2 && test01[i][j] == test01[i][j + 1] && test01[i][j] == test01[i][j + 2]) {
 
+        //console.log("Largo eje i= " + test01.length + " \nLargo eje j= " + test01[i].length)
         comentario = "Anomalia en la letra " + test01[i][j] + " | Posición " + "i=" + i + " j=" + j + " | Orientación horizontal";
         console.log(comentario);
 
+        anomalia = true;
+        res.status(200)
         return res.json({
           mensaje: comentario
         })
 
       }
       //Búsqueda de anomalías en Orientación vertical
-      if (test01[i][j] == test01[i + 1][j] && test01[i][j] == test01[i + 2][j]) {
-        console.log("Largo eje i= " + test01.length + " \nLargo eje j= " + test01[i].length)
+      if (test01.length > i + 2 &&
+        test01[i][j] == test01[i + 1][j] &&
+        test01[i][j] == test01[i + 2][j]) {
 
+        //console.log("Largo eje i= " + test01.length + " \nLargo eje j= " + test01[i].length)
         comentario = "Anomalia en la letra " + test01[i][j] + " | Posición " + "i=" + i + " j=" + j + " | Orientación vertical";
         console.log(comentario);
 
+        anomalia = true;
+        res.status(200)
         return res.json({
+          code: 200,
           mensaje: comentario
         })
 
       }
       //Búsqueda de anomalías en Orientación diagonal descendente
-      if (test01[i][j] == test01[i + 1][j + 1] && test01[i][j] == test01[i + 2][j + 2]) {
-        console.log("Largo eje i= " + test01.length + " \nLargo eje j= " + test01[i].length)
+      if (test01.length > i + 2 && test01[i].length > j + 2 &&
+        test01[i][j] == test01[i + 1][j + 1] &&
+        test01[i][j] == test01[i + 2][j + 2]) {
 
+        //console.log("Largo eje i= " + test01.length + " \nLargo eje j= " + test01[i].length)
         comentario = "Anomalia en la letra " + test01[i][j] + " | Posición " + "i=" + i + " j=" + j + " | Orientación diagonal descendente";
         console.log(comentario);
 
+        anomalia = true;
+        res.status(200)
         return res.json({
           mensaje: comentario
         })
@@ -93,43 +88,40 @@ app.post('/validate-anomaly', function (req, res) {
       }
 
       //Búsqueda de anomalías en Orientación diagonal ascendente
-      if (test01[i][j] == test01[i + 1][j - 1] && test01[i][j] == test01[i + 2][j - 2]) {
+      if (test01.length > i + 2 && test01[i].length > j - 2 &&
+        test01[i][j] == test01[i + 1][j - 1] &&
+        test01[i][j] == test01[i + 2][j - 2]) {
 
-        console.log("Largo eje i= " + test01.length + " \nLargo eje j= " + test01[i].length)
+        //console.log("Largo eje i= " + test01.length + " \nLargo eje j= " + test01[i].length)
         comentario = "Anomalia en la letra " + test01[i][j] + " | Posición " + "i=" + i + " j=" + j + " | Orientación diagonal ascendente";
         console.log(comentario);
 
+        anomalia = true;
+        res.status(200)
         return res.json({
           mensaje: comentario
         })
 
       }
-      // else {
-      //   //Sin anomalías
-      //   comentario = "No se encontraron anomalías"
-      //   console.log(comentario);
-      //   return res.json({
-      //     mensaje: comentario
-      //   })
-      // }
 
 
 
-
+      
 
     }
 
+  }
 
+  function actualizarBD() {
+        
 
   }
 
+  return res.json({
+    mensaje: comentario
+  })
 
-  // let i = 0;
-  // let j = 0;
 
-
-  //console.log(res.json.mensaje)
-  //res.json({ mensaje: '¡A beber cerveza!' }) 
 
 })
 
